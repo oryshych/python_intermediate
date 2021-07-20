9c7a48e4e7487badcd06cad4eebfd0b3f5277009
oryshych <84316457+oryshych@users.noreply.github.com>
2021-07-20 17:49:12 +0300

    GUI_Turtle

---
+++ day-018-hirst-painting/main.py
@@ -0,0 +1,51 @@
+### Part1
+### import colours from image
+
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_colour = (r, g, b)
+#     rgb_colors.append(new_colour)
+
+# print(rgb_colors)
+# colour_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+### Part2
+### make 10x10 dots paint, dot = 20, space = 50
+
+import turtle as turtle_module
+import random
+
+colour_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+tim = turtle_module.Turtle()
+turtle_module.colormode(255)
+tim.speed(0)
+
+
+def line():
+    for _ in range(10):
+        tim.dot(20, random.choice(colour_list))
+        tim.penup()
+        tim.forward(50)
+        tim.pendown()
+
+
+def draw():
+    vertical = -220
+    for _ in range(10):
+        tim.penup()
+        tim.setposition(-220, vertical)
+        line()
+        vertical += 50
+
+
+draw()
+
+screen = turtle_module.Screen()
+screen.exitonclick()
